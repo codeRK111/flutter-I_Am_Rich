da9eae2875e1fa50846afe0388b649ec28c5641d
rakesh <rakesh.das@idslogic.com>
2019-07-09 11:15:20 +0530

    I am rich completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,20 @@
-// Flutter code sample for material.Scaffold.1
-
-// This example shows a [Scaffold] with an [AppBar], a [BottomAppBar] and a
-// [FloatingActionButton]. The [body] is a [Text] placed in a [Center] in order
-// to center the text within the [Scaffold] and the [FloatingActionButton] is
-// centered and docked within the [BottomAppBar] using
-// [FloatingActionButtonLocation.centerDocked]. The [FloatingActionButton] is
-// connected to a callback that increments a counter.
-
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Center(
-      child: Text("I am rich"),
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("I am rich"),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      backgroundColor: Colors.blue[200],
+      body: Center(
+        child: Image(
+          image: NetworkImage(
+            "https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+          ),
+        ),
+      ),
     ),
   ));
 }
